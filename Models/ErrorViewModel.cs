345ad723fb655abcde2f14631a99f79d6cc5b6c9
moshe-druk <dmd6708713@gmail.com>
2024-08-20 13:42:32 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Military_Asset_Management_client.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
