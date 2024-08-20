7218b8fa8ed6663e9f925232bf9654d99d95c4c7
moshe-druk <dmd6708713@gmail.com>
2024-08-20 13:43:23 +0300

    new

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,7 +12,6 @@ namespace Military_Asset_Management_client.Controllers
         {
             _logger = logger;
         }
-
         public IActionResult Index()
         {
             return View();
